e721afae0c5eb0e754464178cc36d528e8849ded
hsu3113 <gana31133113@gmail.com>
2024-09-21 19:47:13 +0900

    .

---
+++ 3/3.ino
@@ -0,0 +1,29 @@
+#define PIN_LED 9
+
+void setup() {
+    pinMode(PIN_LED, OUTPUT);
+    Serial.begin(115200); // Initialize serial port
+    while (!Serial) {
+        ; // wait for serial port to connect.
+    }
+}
+
+void loop() {
+    digitalWrite(PIN_LED, 0);
+    delay(1000);
+    blink_led(5);
+    digitalWrite(PIN_LED, 1);
+    while(1)
+    {
+    }
+}
+
+void blink_led(int count) {
+    for(int i = 0; i < count; i ++){
+      digitalWrite(PIN_LED, 1);
+      delay(100);
+      digitalWrite(PIN_LED, 0);
+      delay(100);
+    }
+    return;
+}
